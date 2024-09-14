b1462521b81802a361d6943cb8d6d0a1c0e3d30f
simesaba80 <simesaba80>
2024-09-15 02:42:36 +0900

    テーブル作成用スクリプトの作成

---
+++ backend/reset.py
@@ -0,0 +1,15 @@
+from backend.db import Base, engine
+
+# 接続テスト
+try:
+    connection = engine.connect()
+    print("データベースへの接続に成功しました！")
+except Exception as e:
+    print(f"データベースへの接続エラー: {e}")
+finally:
+    connection.close()
+
+# データベースのテーブルを削除
+Base.metadata.drop_all(bind=engine)
+# データベースのテーブルを作成
+Base.metadata.create_all(bind=engine)

351b7e5c59fc90d9d94777f1837a84e446b40f67
simesaba80 <simesaba80>
2024-09-16 00:59:46 +0900

    テーブル定義の変更

--- backend/models/color.py
+++ backend/models/color.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -8,18 +8,29 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, nullable=False)
-    password = Column(String, nullable=False)
-    email = Column(String, nullable=False)
-    eisafile = Column(String, nullable=False)
+    user_id = Column(String, primary_key=True)
+    mail = Column(String, nullable=False, unique=True)
+    hashed_password = Column(String, nullable=False)
+    name = Column(String, nullable=False)
     Color = relationship("Color", back_populates="users")
+    Eisafile = relationship("Eisafile", back_populates="users")
+
+
+class Eisafile(Base):
+    __tablename__ = "eisafiles"
+
+    id = Column(String, ForeignKey("users.user_id"), primary_key=True, index=True)
+    file_path = Column(String, nullable=False)
+    created_at = Column(String, nullable=False)
+    updated_at = Column(String, nullable=False)
+
+    users = relationship("User", back_populates="Eisafile")
 
 
 class Color(Base):
     __tablename__ = "colors"
 
-    id = Column(Integer, ForeignKey("users.id"), primary_key=True, index=True)
+    id = Column(String, ForeignKey("users.user_id"), primary_key=True, index=True)
     color1 = Column(String, nullable=False)
     color2 = Column(String, nullable=False)
 

2269482414d80b940989de30217e8278d8d9c3e6
simesaba80 <simesaba80>
2024-09-16 00:59:56 +0900

    sqlの変更

--- backend/cruds/color.py
+++ backend/cruds/color.py
@@ -1,19 +1,19 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
-from backend.models.color import Color, User
+from backend.models.color import Color, Eisafile, User
 
 
-def search_user(db: Session, email: str, password: str):
+def search_user(db: Session, mail: str, hashed_password: str):
     return (
         db.query(User)
-        .filter(User.email == email)
-        .filter(User.password == password)
+        .filter(User.mail == mail)
+        .filter(User.hashed_password == hashed_password)
         .first()
     )
 
 
-def registar_color(db: Session, id: int, color1: str, color2: str):
+def registar_color(db: Session, id: str, color1: str, color2: str):
     new_color = Color(
         id=id,
         color1=color1,
@@ -29,7 +29,7 @@ def registar_color(db: Session, id: int, color1: str, color2: str):
     return new_color
 
 
-def update_color(db: Session, id: int, color1: str, color2: str):
+def update_color(db: Session, id: str, color1: str, color2: str):
     record = db.query(Color).filter(Color.id == id).first()
     if record is None:
         raise HTTPException(status_code=404, detail="Record not found")
@@ -38,3 +38,10 @@ def update_color(db: Session, id: int, color1: str, color2: str):
     db.commit()
     db.refresh(record)
     return record
+
+
+def get_filepath_by_id(db: Session, id: str):
+    eisa_file = db.query(Eisafile).filter(Eisafile.id == id).first()
+    if eisa_file is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return eisa_file.file_path
--- backend/router/color.py
+++ backend/router/color.py
@@ -9,6 +9,7 @@ from backend.models.color import Color
 from backend.services.sound_research import sound_research
 from backend.utils.decode import get_payload_from_token
 from backend.utils.download import download_blob
+from backend.utils.makecolor import feature_to_color
 
 router = APIRouter()
 
@@ -24,16 +25,16 @@ async def get_all(db=Depends(get_db)):
 @router.get("/color/get", response_model=color_schema.ColorResponse)
 async def matching(token: str = Depends(oauth2_scheme), db=Depends(get_db)):
     payload = get_payload_from_token(token)
-    email = payload.get("email")
-    password = payload.get("password")
-    user_data = color_cruds.search_user(db, email, password)
+    mail = payload.get("mail")
+    hashed_password = payload.get("hashed_password")
+    user_data = color_cruds.search_user(db, mail, hashed_password)
     if user_data is None:
         raise HTTPException(status_code=404, detail="User not found")
-    color_data = db.query(Color).filter(Color.id == user_data.id).first()
+    color_data = db.query(Color).filter(Color.id == user_data.user_id).first()
     if color_data is None:
         raise HTTPException(status_code=404, detail="Record not found")
     return color_schema.MatchingResponse(
-        color1=color_data.color1, color2=color_data.color2
+        id=color_data.id, color1=color_data.color1, color2=color_data.color2
     )
 
 
@@ -41,29 +42,18 @@ async def matching(token: str = Depends(oauth2_scheme), db=Depends(get_db)):
 async def recording(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     # Assuming you have a database session and models set up
     payload = get_payload_from_token(token)
-    email = payload.get("email")
-    password = payload.get("password")
-    user_data = color_cruds.search_user(db, email, password)
+    mail = payload.get("mail")
+    hashed_password = payload.get("hashed_password")
+    user_data = color_cruds.search_user(db, mail, hashed_password)
     if user_data is None:
         raise HTTPException(status_code=404, detail="User not found")
-    download_blob(
-        "yanbaru-eisa-storage-bucket-prod", user_data.eisafile, "/tmp/hoge.ogg"
-    )
+    file_path = color_cruds.get_filepath_by_id(db, user_data.user_id)
+    download_blob("yanbaru-eisa-storage-bucket-prod", file_path, "/tmp/hoge.ogg")
     feature = sound_research("/tmp/hoge.ogg")
-    color1_data = (
-        "#"
-        + format(feature.speech_rate * 16, "x")
-        + format(feature.pitch * 16, "x")
-        + format(feature.syllable_1 * 16, "x")
+    color1_data, color2_data = feature_to_color(feature)
+    color_data = color_cruds.registar_color(
+        db, user_data.user_id, color1_data, color2_data
     )
-    color2_data = (
-        "#"
-        + format(feature.syllable_2 * 16, "x")
-        + format(feature.syllable_3 * 16, "x")
-        + format(feature.syllable_4 * 16, "x")
-    )
-    color_data = color_cruds.registar_color(db, user_data.id, color1_data, color2_data)
-    print(payload.get("user_id"))
     return color_schema.ColorResponse(
         id=color_data.id, color1=color_data.color1, color2=color_data.color2
     )
@@ -75,18 +65,21 @@ async def get_recording(
     db: Session = Depends(get_db),
 ):
     payload = get_payload_from_token(token)
-    email = payload.get("email")
-    password = payload.get("password")
+    mail = payload.get("mail")
+    hashed_password = payload.get("hashed_password")
     try:
-        user_data = color_cruds.search_user(db, email, password)
+        user_data = color_cruds.search_user(db, mail, hashed_password)
         if user_data is None:
             raise HTTPException(status_code=404, detail="User not found")
-        record_id = user_data.id
+        record_id = user_data.user_id
         record = db.query(Color).filter(Color.id == record_id).first()
         if record is None:
             raise HTTPException(status_code=404, detail="Record not found")
         # Update the record as needed
-        record.color1 = "#FF0000"  # Example update
+        file_path = color_cruds.get_filepath_by_id(db, user_data.user_id)
+        download_blob("yanbaru-eisa-storage-bucket-prod", file_path, "/tmp/hoge.ogg")
+        feature = sound_research("/tmp/hoge.ogg")
+        color1_data, color2_data = feature_to_color(feature)
         updated_record = color_cruds.update_color(
             db, record.id, record.color1, record.color2
         )

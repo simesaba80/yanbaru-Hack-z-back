d8ddd6aaa12586ec6d9db8784a504bb0226e4266
simesaba80 <simesaba80>
2024-09-15 15:18:24 +0900

    デコード関数の分割

--- backend/router/voice.py
+++ backend/router/voice.py
@@ -1,40 +1,15 @@
-import base64
-import json
-
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 
 import backend.schema.voice as voice_schema
 from backend.db import get_db
 from backend.models.voice import Record, User
+from backend.utils.decode import get_payload_from_token
 
 router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-# Base64デコード用の関数
-def base64_decode(data: str):
-    # Base64 URLセーフデコードを行う
-    padding_needed = 4 - (len(data) % 4)
-    data += "=" * padding_needed  # パディングを追加
-    return base64.urlsafe_b64decode(data)
-
-
-# JWTトークンからペイロード部分をデコードしてJSONにする関数
-def get_payload_from_token(token: str):
-    try:
-        # トークンをドットで分割し、ペイロード部分を取り出す
-        payload_base64 = token.split(".")[1]
-        decoded_payload = base64_decode(payload_base64)
-        payload_json = json.loads(decoded_payload)
-        return payload_json
-    except (IndexError, json.JSONDecodeError):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Invalid token format",
-        )
 
 
 @router.post("/recording", response_model=voice_schema.RecordingResponse)
